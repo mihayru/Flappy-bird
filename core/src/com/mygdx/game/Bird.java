bc365f4285d6377d0f08f0a03b02f65d03362f45
mihayru <mjgang258@gmail.com>
2019-12-17 11:55:09 +0200

    added a restartbtn

--- core/src/com/mygdx/game/Bird.java
+++ core/src/com/mygdx/game/Bird.java
@@ -12,27 +12,29 @@ public class Bird {
     float vy;
     float gravity;
 
-    public  Bird(){
+
+    public Bird(){
         img = new Texture("bird1.png");
         position = new Vector2(100,380);
         vy = 0;
-        gravity = -0.7f;
-
+        gravity = - 0.7f;
     }
 
     public void render(SpriteBatch batch){
-        batch.draw(img, position.x,position.y);
-
+        batch.draw(img, position.x, position.y);
     }
 
     public void update(){
 
-        if(Gdx.input.isKeyPressed(Input.Keys.SPACE)){
-            vy = 8;
+        if (Gdx.input.isKeyPressed(Input.Keys.SPACE)){
+            vy = 10;
         }
         vy += gravity;
         position.y += vy;
+    }
 
-
+    public void recreate(){
+        position = new Vector2(100,380);
+        vy = 0;
     }
 }
